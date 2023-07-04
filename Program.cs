e9e643d4507b5e2d9e8579b6cee1291280284ada
Pradim22 <792616@mail.ru>
2023-07-04 20:44:37 +0530

    Create Methods

--- Program.cs
+++ Program.cs
@@ -1,27 +1,71 @@
 ﻿/* Программа из имеющегося массива строк формирует 
 массив из строк, длина которых меньше или равно 3 символа. */
 
-String[] arrWords = {"f24f", "hjhjhj", "kjlsld", "342", ":))))"};
-int count = 0;
-int i = 0;
+String[] arrWords = {"Kazan'", "Tyumen", "Gasnn", "2007", "nn:-)"};
 
-for (i=0; i < arrWords.Length; i++){
-    if (arrWords[i].Length <= 3){
-        count++;
+int count = ArrCount(0, arrWords);
+String[] selectArray = СreatureSelectArray(count, 0, arrWords);
+PrintArray (count, arrWords, selectArray);
+
+int ArrCount (int cnt, String[]arr)
+{
+    for (int i=0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            cnt++;
+        } 
+    }
+    return cnt;
+}
+
+String[] СreatureSelectArray(int cnt, int n, String[]arr)
+{
+    String[] selectWords = new String [cnt];
+    // if (cnt == 0)
+    // {
+    //     PrintArray(arr);
+    // } else 
+    if (cnt == 0) return selectWords;
+    else 
+    {
+        for (int i=0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3)
+            {
+                selectWords[n] = arr[i];
+                n++;
+            }
+        }
+    }
+    return selectWords;
+}
+
+void PrintArray(int cnt, String[] arr, String[] selectarr)
+{
+    Console.WriteLine();
+    if (cnt == 0)
+    {
+        Console.Write("[");
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+    Console.WriteLine($"{arr[arr.Length - 1]}] --> [] ");
+    Console.WriteLine();
+    } else
+    {
+        Console.Write("[");
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+    Console.Write($"{arr[arr.Length - 1]}] --> [");
+        for (int i = 0; i < selectarr.Length - 1; i++)
+        {
+            Console.Write($"{selectarr[i]}, ");
+        }
+    Console.WriteLine($"{arr[selectarr.Length - 1]}]");
+    Console.WriteLine();
     }
 }
-    if (count == 0){
-        Console.WriteLine(arrWords + " -> []");
-    }
-// Console.WriteLine(arrWords[0].Length);
-
-// for (int i = 0; i < arrWords.Length; i++)
-// {
-//     int n = arrWords.Length;
-//     int countNewArr = 0;
-//     String[] selectWold = new String[n];
-
-//   if (arrWords[i].Length <= 3){
-//     selectWold [m-(m-i)]  = arrWords[i];
-//   }
-// }
