7fc9aee9c7e58c41586e29de1038a59eb81037ca
Pradim22 <792616@mail.ru>
2023-07-04 16:29:28 +0530

    redy 1 cycle

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
-﻿String[] arrWords = new String[];
-arrWords [0] = ["abr"];
-Console.WriteLine("Hello, World!");
+﻿/* Программа из имеющегося массива строк формирует 
+массив из строк, длина которых меньше или равно 3 символа. */
+
+String[] arrWords = {"f24f", "hjhjhj", "kjlsld", "342", ":))))"};
+int count = 0;
+int i = 0;
+
+for (i=0; i < arrWords.Length; i++){
+    if (arrWords[i].Length <= 3){
+        count++;
+    }
+}
+    if (count == 0){
+        Console.WriteLine(arrWords + " -> []");
+    }
+// Console.WriteLine(arrWords[0].Length);
+
+// for (int i = 0; i < arrWords.Length; i++)
+// {
+//     int n = arrWords.Length;
+//     int countNewArr = 0;
+//     String[] selectWold = new String[n];
+
+//   if (arrWords[i].Length <= 3){
+//     selectWold [m-(m-i)]  = arrWords[i];
+//   }
+// }
